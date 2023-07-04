fe6b04b51db017211a1f2d13c588948423f0323a
SGrommelt <onehappeeshala@gmail.com>
2023-07-03 22:33:33 -0600

    DATA BASE: Model testing.

--- api/tests/routes/videogame.spec.js
+++ api/tests/routes/videogame.spec.js
@@ -7,6 +7,11 @@ const { Videogame, conn } = require('../../src/db.js');
 const agent = session(app);
 const videogame = {
   name: 'Super Mario Bros',
+  description: "Italian twin brother plumbers Mario and Luigi exterminate creatures emerging from the sewers by knocking them upside-down and kicking them away.",
+  platforms: "PC, Console",
+  image: "https://people.com/thmb/cK_RpydkLMmjxyqe3vOqksRVXuI=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(719x207:721x209)/super-mario-bros-2026fec1a6fc4e1fbbe6f0bbb3adedab.jpg",
+  releaseDate: "1983",
+  rating: 5
 };
 
 describe('Videogame routes', () => {
