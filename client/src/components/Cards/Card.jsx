b44cd6b9d7a908ba63f444c84d80eb89a50ac197
SGrommelt <onehappeeshala@gmail.com>
2023-07-05 12:24:15 -0600

    FRONT END: Fixed name length bug.

--- client/src/components/Cards/Card.jsx
+++ client/src/components/Cards/Card.jsx
@@ -3,11 +3,30 @@ import styles from './Cards.module.css';
 import { Link } from 'react-router-dom';
 
 export default function Card(props) {
+
+  const nameLength = (name) => {
+    if(name.length > 35) {
+      const arr = name.split(" ");
+      if(arr !== undefined) {
+        let total = 0;
+        for (let i=0; i<arr.length; i++) {
+          total = total + arr[i].length;
+          if(total > (35 - (arr.length-1))) {
+            arr.splice(i);
+            break;
+          }
+        }
+        return arr.join(" ");
+      }
+    }
+    return name;
+  }
+
   return (
     <div className={styles.card}>
       <div className={styles.cardInfo} style={props.style}>
         <Link to={`/detail/${props.id}`}>
-          <h2 className={styles.name}>{props.name}</h2>
+          <h2 className={styles.name}>{nameLength(props.name)}</h2>
           {/* {props.genres.map( genre => {return <h3>{genre}</h3>})} */}
         </Link>
       </div>
